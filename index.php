702f10be406894203eff0d3e38f0880be01d8e93
Anderson Gonçalves <31591711+Andersonx2@users.noreply.github.com>
2022-08-19 00:24:09 -0300

    criando a classe page, e removendo arquivos desnecessarios

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php 
 
 require_once("vendor/autoload.php");
+use \Slim\Slim; 
+use \Hcode\Page;
 
 $app = new \Slim\Slim();
-
 $app->config('debug', true);
 
-$app->get('/', function() {
-    
-	$sql = new \Hcode\DB\sql();
-	$results = $sql->select("SELECT * FROM tb_users"); 
 
-	echo json_encode($results);
+$app->get('/', function() {
+
+	
+	$page = new Page();
+	$page->setTpl("index");
 
 
 });
