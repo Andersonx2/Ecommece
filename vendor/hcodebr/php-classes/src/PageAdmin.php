9def46c824ae9a0a80316b2548a686521a245933
Anderson Gonçalves <31591711+Andersonx2@users.noreply.github.com>
2022-08-31 19:37:15 -0300

    adicionando pageAdmin

---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,19 @@
+<?php 
+namespace Hcod;
+
+use Hcode\Page;
+
+class PageAdmin extends Page { 
+
+public function __construct($opts= array(),$tpl_dir= "/view/admin"){ 
+
+parent::__construct($opts,$tpl_dir);
+
+
+} 
+    
+}
+
+
+
+?> 
